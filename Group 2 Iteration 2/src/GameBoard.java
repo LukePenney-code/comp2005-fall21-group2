aa4386055137880da6c68d631f5a891e735d3cfe
LukePenney-code <ljpenney@mun.ca>
2021-10-26 01:01:15 -0230

    Part2

--- Group 2 Iteration 2/src/GameBoard.java
+++ Group 2 Iteration 2/src/GameBoard.java
@@ -1,4 +1,62 @@
+import java.awt.*;
 
-public class GameBoard {
+import javax.swing.*;
+import javax.swing.border.Border;
 
+public class GameBoard extends JFrame {
+	
+    private JPanel topPanel;
+    private JPanel leftPanel;
+    private JPanel rightPanel;
+    private JPanel bottomPanel;
+    private JPanel gameBoard;
+    private GameSpace [][] gameSpaces;
+    private int numPlayers, rows, columns;
+    
+    
+    public GameBoard(){
+    	
+    	rows = 5;
+    	columns = 5;
+    	
+    	topPanel = new JPanel();
+    	//bottomPanel = new JPanel();
+    	//rightPanel = new JPanel();
+    	//leftPanel = new JPanel();
+    	gameBoard = new JPanel();
+    	
+    	this.setSize(500,500);
+    	
+    	topPanel.setLayout(new FlowLayout());
+    	topPanel.setSize(200,200);
+    	//bottomPanel.setLayout(new FlowLayout());
+    	
+    	gameBoard.setLayout(new GridLayout());
+    	gameBoard.setSize(500,500);
+    	
+    	gameSpaces = new GameSpace[rows][columns];
+    	for (int x = 0 ; x < columns; x++) {
+    		for ( int y = 0; y < rows; y++) {
+    			Border raisedBevel = BorderFactory.createRaisedBevelBorder();
+    			gameSpaces[x][y] = new GameSpace(x, y);
+    			gameSpaces[x][y].setSize(20,20);
+    			gameSpaces[x][y].setOpaque(true);
+    			gameSpaces[x][y].setBorder(raisedBevel);
+    			gameBoard.add(gameSpaces[x][y]);
+    		}
+    	}
+    	getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(topPanel, BorderLayout.NORTH);
+		//getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+		//getContentPane().add(rightPanel, BorderLayout.EAST);
+		//getContentPane().add(leftPanel, BorderLayout.WEST);
+		getContentPane().add(gameBoard, BorderLayout.CENTER);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setResizable(false);
+		setVisible(true);
+    }
+
+
+	
 }
--- Group 2 Iteration 2/src/GamePiece.java
+++ Group 2 Iteration 2/src/GamePiece.java
@@ -1,4 +1,34 @@
+import java.awt.*;
 
-public class GamePiece {
-
+import javax.swing.*;
+public class GamePiece extends JButton{
+	
+	private Color color;
+	private int playerNum;
+	/**
+	 * @param xCoord
+	 * @param yCoord
+	 * @param color
+	 * @param playerNum
+	 * @param color 
+	 */
+	public GamePiece(Color color) {
+		
+		this.color = color;
+		playerNum = 0;
+		this.setSize(20,20);
+	}
+	
+	public int getPlayerNum() {
+		return playerNum;
+	}
+	public void setPlayerNum(int playerNum) {
+		this.playerNum = playerNum;
+	}
+	public Color getColor() {
+		return color;
+	}
+	public void setColor(Color newColor) {
+		this.color = newColor;
+	}
 }
